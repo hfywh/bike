ccde477bb8d785a66e37b240b2146165c45f4270
hfywh <hfywy.nottingham.edu.my>
2021-11-19 11:52:19 +0800

    initial

---
+++ src/com/theBestBikeShop/Bicycle.java
@@ -0,0 +1,41 @@
+package com.theBestBikeShop;
+
+public class Bicycle {
+        private int gear;
+        private int speed;
+        private boolean lightstatus;
+
+    public Bicycle(int gear) {
+        this.gear = gear;
+        this.lightstatus=false;
+    }
+
+    //modify a file
+        public void setGear(int newValue) {
+            gear=newValue;
+        }
+
+        public void applyBrake(int decrement) {
+            speed-=decrement;
+        }
+
+        public void speedUp(int increment) {
+            speed+=increment;
+        }
+
+        public void switchLightStatus(){
+            if(lightstatus){
+                lightstatus=false;
+            }else{
+                lightstatus=true;
+            }
+        }
+
+        public void currentState(){
+            System.out.println("Gear:"+gear+"; Speed:"+speed+"; LightStatus:"+lightstatus);
+        }
+
+    public int getgear() {
+        return gear;
+    }
+}
